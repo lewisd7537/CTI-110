2143a9f9075ff8cbe5f8e1a33f04c7641c0be397
lewisd7537 <lewisd7537@student.faytechcc.edu>
2025-02-13 14:03:39 -0500

    Add files via upload

---
+++ P1HW1_Lewis.py
@@ -0,0 +1,44 @@
+ # Your Name
+ # Date
+ # Assignment Name
+ # A brief description of the project
+
+print("-----Calculating Exponents-----")
+print("\n") # newline character
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+print("\n") # newline character
+value = base ** exponent
+value = pow(base, exponent)
+#value = math.pow(base,exponent)
+print(base,"raised to the power of",exponent,"is",value,"!!")
+
+print("-----Addition and Subtraction-----")
+print("\n") # newline character
+
+num1 = int(input("Enter an starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+print("\n") # newline character
+num4 = num1 + num2 - num3 
+#value = math.pow(num1,num2,num3)
+print(num1, "+", num2,"-",num3, "is equal to", num4)
+
+##input("Press any key to continue!!")
+
+
+
+
+
+
+
+##num2 = int(input("Enter another number:"))
+
+##num3 = num1 + num2
+
+##print(num1, "+", num2,"=",num3)
+
+
+input("Press any key to continue!!")
