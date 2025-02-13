c7f84537bfe95a0216e89a9cc4421db3e5b49f7f
lewisd7537 <lewisd7537@student.faytechcc.edu>
2025-02-13 14:03:12 -0500

    Add files via upload

---
+++ P1HW2_Lewis.py
@@ -0,0 +1,48 @@
+ # Your Name
+ # Date
+ # Assignment Name
+ # A brief description of the project
+
+print("This program calculates and dispalys travel expenses")
+print("\n") # newline character
+budget = int(input("Enter Budget: "))
+print("\n") # newline character
+destination = str(input("Enter your travel destination: "))         
+print("\n") # newline character
+gas = int(input("How much do you think you will spend on gas?: "))
+print("\n") # newline character
+hotel = int(input("Approximately, how much will you need for accomodation/hotel?: "))
+print("\n") # newline character         
+food = int(input("Last, how much do you need for food?: "))
+print("\n") # newline character
+
+print("-"*12,"Travel Expenses","-"*12)
+print("Location:",destination)
+budget = int(input("Initial Budget: "))
+print("\n") # newline character
+# calculation
+gas = int(input("Fuel: "))
+hotel = int(input("Accomodation: "))
+food = int(input("Food: "))
+print("\n") # newline character
+balance = budget - (gas + hotel + food)
+print("Remaining Balance:", budget - (gas + hotel + food))
+
+
+
+
+
+##input("Press any key to continue!!")
+
+##Puesdocode 
+##Ask user to enter their budget
+##Ask user to enter travel destination
+##Ask user for amount they will spend on gas
+##Ask user for amound they will spend on accomodation
+##Ask user for amount they will spend on food
+##Add expenses
+##Subtract expenses from budget
+
+
+
+input("Press any key to continue!!")
