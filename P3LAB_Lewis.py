dae74a2c2678f8da0abecffbf8783c6aa64b9687
lewisd7537 <lewisd7537@student.faytechcc.edu>
2025-03-06 14:58:51 -0500

    Add files via upload

---
+++ P3LAB_Lewis.py
@@ -0,0 +1,73 @@
+ #Your Name
+ #Date
+ #Assignment Name
+ #Brief description of program
+
+# get float from the user
+change = float(input("Enter the amount of money as a float: "))
+# convert the float to an integer
+change = int(change * 100)
+
+if change == 0:
+    print("No change Due!")
+# calculate change for each coin type
+# integer division //
+num_dollars = change // 100
+change = change - (num_dollars * 100)
+
+num_quarters = change // 25
+change = change - (num_quarters * 25)
+
+num_dimes = change // 10
+change = change - (num_dimes * 10)
+
+num_nickels = change // 5
+change = change - (num_nickels * 5)
+
+num_pennies = change // 1
+change = change - (num_pennies * 1)
+
+# display amounts used
+
+if num_dollars > 0:
+    print(num_dollars, end=' ')
+    if num_dollars == 1:
+        print("Dollar")
+    else:
+        print("Dollars")
+
+if num_quarters > 0:
+    print(num_quarters, end=' ')
+    if num_quarters == 1:
+        print("Quarter")
+    else:
+        print("Quarters")
+
+if num_dimes > 0:
+    print(num_dimes, end=' ')
+    if num_dimes == 1:
+        print("Dime")
+    else:
+        print("Dimes")
+
+if num_nickels > 0:
+    print(num_nickels, end=' ')
+    if num_nickels == 1:
+        print("Nickel")
+    else:
+        print("Nickels")
+
+if num_pennies > 0:
+    print(num_pennies, end=' ')
+    if num_pennies == 1:
+        print("Penny")
+    else:
+        print("Pennies")
+
+
+
+
+
+
+
+
